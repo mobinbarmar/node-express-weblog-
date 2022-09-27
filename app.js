3b611609418ea5221fe518f5db6dea6f484e108f
Mr.mobin <barmar.mobin@gmail.com>
2022-09-27 18:24:07 +0330

    index.ejs, routes added

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+//* Internal Modules
+const path = require('path');
+
+//* External Modules
 const express = require('express');
+
+//* My Modules
+const indexRoutes = require('./routes/index');
 
 const app = express()
 
 
+//* View Engine
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+
+//* Statics
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//* Routes
+app.use(indexRoutes)
 
 
 
