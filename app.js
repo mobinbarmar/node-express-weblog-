39fad0068f9637059e145e21961d4eeecde4b8bb
Mr.mobin <barmar.mobin@gmail.com>
2022-09-27 23:50:30 +0330

    dev dependencies added

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 //* External Modules
 const express = require('express');
+const dotEnv = require('dotenv');
 
 //* My Modules
 const indexRoutes = require('./routes/index');
+
+//*Load config
+dotEnv.config({ path: './config/config.env' })
 
 const app = express()
 
@@ -23,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(indexRoutes)
 
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log('http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}  \n{in ${process.env.NODE_ENV} mode!}`);
 })
