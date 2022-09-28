ff6293fb4a2ce8d56975ac9a2f0ebfdd15035d04
Mr.mobin <barmar.mobin@gmail.com>
2022-09-28 18:24:36 +0330

    Yup added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const Yup = require('yup');
 
 const router = express.Router()
+
+const schema = Yup.object().shape({
+    fullname: Yup.string().required('نام و نام خانوادگی الزامی میباشد').min(4, 'بیش از 4 کارکتر وارد کنید').max(255, 'کمتر از 255 کارکتر موردنیاز است'),
+    email: Yup.string().email('ایمیل معتبر نمیباشد').required('ایمیل الزامی میباشد'),
+    password: Yup.string().min(4, 'بیش از 4 کارکتر وارد کنید').max(255, 'کمتر از 255 کارکتر موردنیاز است').required('پسورد الزامی میباشد'),
+    confirmPassword: Yup.string().required('پسورد الزامی میباشد').oneOf([Yup.ref('password'), null])
+})
 
 //* Login Page
 router.get('/login', (req, res) => {
@@ -14,8 +22,11 @@ router.get('/register', (req, res) => {
 
 //* register Handle
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('weblog')
+    schema.validate(req.body).then(() => {
+        res.redirect('/users/login')
+    }).catch((err) => {
+        res.render('register', {pageTitle:'ثبت نام', path: '/register', errors: err.errors})
+    })
 })
 
 module.exports = router
